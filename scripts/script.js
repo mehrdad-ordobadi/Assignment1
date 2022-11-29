cf506b592ea235d1dcf47b9ef287cd83774ff620
mehrdad <mehrdad.ordobadi@gmail.com>
2022-11-28 18:13:05 -0800

    add ability to toggle btw light and dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,38 @@
 const darklightmode = document.querySelector('.darktheme')
-// console.log(darklightmode)
+
+
 function isDark(e){
     // console.log(e.target)
-    if(e.target.classList.contains('darktheme')){
+
+    if(e.target.textContent === 'Dark Theme'){
         turnDark()
     }
-    else if(e.target.classList.contains('lightheme')){
+    else if(e.target.textContent === 'Light Theme'){
         turnLight()
     }
 }
 function turnDark(){
+    mybutton = document.querySelector('.darktheme')
     toggleClasses('.mainheadfooter','.mainheadfooterDark')
-    toggleClasses('.textareastyle','textareastyleDark')
+    toggleClasses('.textareastyle','.textareastyleDark')
     toggleClasses('.asidestyle','.asidestyleDark')
     toggleClasses('.newnote','newnoteDark')
     toggleClasses('.savebutton','.savebuttonDark')
     toggleClasses('.darktheme','.lighttheme')
-    
+    mybutton.textContent = 'Light Theme'  
 }
+
+function turnLight(){
+    mybutton = document.querySelector('.lighttheme')
+    toggleClasses('.mainheadfooterDark','.mainheadfooter')
+    toggleClasses('.textareastyleDark','.textareastyle')
+    toggleClasses('.asidestyleDark','.asidestyle')
+    toggleClasses('newnoteDark','.newnote')
+    toggleClasses('.savebuttonDark','.savebutton')
+    toggleClasses('.lighttheme','.darktheme')
+    mybutton.textContent = 'Dark Theme'  
+}
+
 function toggleClasses(class1,class2){
     myelem = document.querySelectorAll(class1)
     class1 = class1.replace('.','')
